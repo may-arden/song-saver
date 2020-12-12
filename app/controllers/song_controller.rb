40de3b67267fbe33ed511a19cf4805aa64d326bc
may arden <mayard3n@gmail.com>
2020-12-11 17:24:43 -0800

    started building models

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -1,5 +1,17 @@
 require './config/environment'
 
 class SongController < Sinatra::Base
+    
+    configure do
+        enable :sessions
+        set :sessions, "secret"
+        set :public_folder, "public"
+        set :views, 'app/views/song_views'
+    end
+
+    get '/songs' do
+        @song = Song.find(params[:id])
+        erb :song_edit 
+    end
 
 end
