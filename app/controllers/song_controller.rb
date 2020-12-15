aae529696765fe1c1b5d83fcfdcf86a646feedfb
may arden <mayard3n@gmail.com>
2020-12-15 11:21:21 -0800

    built an interator inside my songs view

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -4,6 +4,12 @@ class SongController < ApplicationController
         erb :'song/songs'
     end
 
+    post '/songs' do
+    @songs = Song.new(:title => params[:title], :genre => params[:genre], :composer => params[:composer])
+    @songs.save
+    redirect to '/songs'
+    end 
+
     get '/create_song' do
         erb :'song/create_song'
     end 
