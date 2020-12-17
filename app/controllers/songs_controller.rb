68b0ae5f3f4f628bfe82352c12b38e7236cdfce4
may arden <mayard3n@gmail.com>
2020-12-17 15:57:59 -0800

    building update functionality

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,11 +18,17 @@ class SongsController < ApplicationController
         redirect '/songs'
     end 
 
-    get '/songs/:id' do
+    get '/songs/show/:id' do
         redirect_if_not_logged_in
         @song = Song.find(params[:id])
         erb :'/songs/show'
     end     
+
+    get '/songs/:id/edit' do
+        redirect_if_not_logged_in
+        @song = Song.find(params["id"])
+        erb :"/songs/edit/"
+    end 
 
 
 
