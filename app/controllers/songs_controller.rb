227aee6a7658d7e8366b6f499e215dc6f04a0f41
may arden <mayard3n@gmail.com>
2021-01-06 11:17:14 -0800

    removed private method in favor of dryer code

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,14 +9,12 @@ class SongsController < ApplicationController
         erb :'/songs/index'
     end
 
-    get '/new' do
-        # '/songs/new' << change
+    get '/songs/new' do
         redirect_if_not_logged_in 
         erb :'/songs/new'
     end 
 
     post '/songs' do
-        # if this new object doesn't have blank parameters, then 
         if (params[:title]) == "" || params[:genre] == "" || params[:composer] == ""
             redirect '/new'
          
@@ -29,15 +27,14 @@ class SongsController < ApplicationController
         end 
     end 
 
-    get '/songs/show/:id' do
-        # change to '/songs/:id'
+    get '/songs/:id' do
         redirect_if_not_logged_in
+        redirect_if_not_correct_user
         @song = Song.find(params[:id])
         erb :'/songs/show'
     end     
 
-    get '/songs/edit/:id' do
-        # change to '/song/:id/edit'
+    get '/songs/:id/edit' do
         redirect_if_not_logged_in
         @song = Song.find(params[:id])
         if @song.user == current_user 
@@ -48,29 +45,21 @@ class SongsController < ApplicationController
     end 
 
     
-    patch '/songs/edit/:id' do
-        # add validation make helper (redirect if not authorized)
+    patch '/songs/:id' do
         @song = Song.find(params[:id])
         @song.update(params[:songs])
         @song.save 
-        erb :'/songs/show'
+        redirect '/songs'
     end 
 
     delete '/songs/:id' do
-        # add validation make helper
+        redirect_if_not_correct_user
         redirect_if_not_logged_in
         @song = Song.find(params[:id])
         @song.destroy
         redirect '/songs'
     end 
 
-private
-
-def redirect_if_not_correct_user(user)
-    if current_user != user
-        redirect '/'
-    end 
-end 
 
 
 end
